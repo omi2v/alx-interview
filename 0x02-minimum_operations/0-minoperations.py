122cbf2b9edaf14c3e2f546043496d03dd1bb142
omi2v <omibedewi@gmail.com>
2023-04-06 05:03:10 -0700

    min operation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-    """
-In a text file, there is a single character H.
-Your text editor can execute only two operations in this file:
-Copy All and Paste. Given a number n, write a method that calculates
-the fewest number of operations needed to result in exactly n
-H characters in the file.
-More info:
-    Prototype: def minOperations(n)
-    Returns an integer
-    If n is impossible to achieve, return 0
-    """
-    def countProcess(num):
+"""
+In a text file, there is a single character H. Your text editor
+can execute only two operations in this file: Copy All and Paste.
+Given a number n, write a method that calculates the fewest number
+of operations needed to result in exactly n H characters in the file.
+"""
+
+
+def countProcess(number):
     """ Return list of process until n H """
     con = 1
     p_list = []
-    val = num
+    val = number
     while val != 1:
         con += 1
         if val % con == 0:
