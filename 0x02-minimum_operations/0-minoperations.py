9639846ef133806617d887e6f6cd3f927dc85c09
omi2v <omibedewi@gmail.com>
2023-04-06 03:05:14 -0700

    min operation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,15 +10,14 @@ More info:
     Returns an integer
     If n is impossible to achieve, return 0
 """
-def minOperations(n):
-    if n == 1:
+def minOperations(n, int):
         return 0
-    dp = [float('int')] * (n+1)
-    dp[1] = 0
-
-    for i in range(2, n+1):
-        for j in range(1, i):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + i//j)
-
-                return dp[n] if dp[n] != float('inf') else 0
+    dp = 0
+    i = 2
+    while(i <= n):
+        if not (n % i):
+            n = int(n / i)
+            dp += i
+            i = 1
+            i += 1
+            return dp
