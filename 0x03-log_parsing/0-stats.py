d1991771796b316408c9cebffdfe47e6b7462b4c
omi2v <omibedewi@gmail.com>
2023-04-13 12:43:30 -0700

    state

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,18 +6,17 @@ import sys
 
 
 if __name__ == "__main__":
-    total_file_size = 0
-    status_codes = {
-            "200": 0,
+    status_codes = {"200": 0,
             "301": 0,
             "400": 0,
             "401": 0,
             "403": 0,
             "404": 0,
             "405": 0,
-            "500": 0,
-            }
+            "500": 0,}
     count = 1
+    total_file_size = 0
+
     def parse_line(line):
         """ Read, parse and grab data"""
         try:
