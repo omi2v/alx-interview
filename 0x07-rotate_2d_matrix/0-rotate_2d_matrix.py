ff1f7bcd89e10e1e8e9bcec7c05d000b4acd1569
omi2v <omibedewi@gmail.com>
2023-06-08 03:07:19 -0700

    Rotate 2D Matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,6 +5,8 @@ Prototype: def rotate_2d_matrix(matrix):
 Do not return anything. The matrix must be edited in-place.
 You can assume the matrix will have 2 dimensions and will not be empty.
 """
+
+
 def rotate_2d_matrix(matrix):
     n = len(matrix)
     # Transpose the matrix
