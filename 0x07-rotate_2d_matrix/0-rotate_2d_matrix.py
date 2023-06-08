6c27e6b9c106ef76014164eeb1d2d7836de927da
omi2v <omibedewi@gmail.com>
2023-06-08 03:06:03 -0700

    2d rote

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-'''
+
+"""
 Prototype: def rotate_2d_matrix(matrix):
 Do not return anything. The matrix must be edited in-place.
-You can assume the matrix will have 2 dimensions and will not be empty
-'''
-
-
+You can assume the matrix will have 2 dimensions and will not be empty.
+"""
 def rotate_2d_matrix(matrix):
-    ''' rotate 2d matrix in place '''
     n = len(matrix)
     # Transpose the matrix
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-            # Reverse each row
-            for i in range(n):
-                matrix[i] = matrix[i][::-1]
+    # Reverse each row
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
