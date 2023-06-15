9858779e07f04649059fbd94517b39d2650cbad0
omi2v <omibedewi@gmail.com>
2023-06-15 00:50:25 -0700

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,9 +7,11 @@ If total is 0 or less, return 0
 If total cannot be met by any number of coins you have, return -1
 coins is a list of the values of the coins in your possession
 The value of a coin will always be an integer greater than 0
-You can assume you have an infinite number of each denomination of coin in the list
+You can assume you have an infinite number of each
+denomination of coin in the list
 Your solution’s runtime will be evaluated in this task
 """
+
 
 def makeChange(coins, total):
     if total <= 0:
